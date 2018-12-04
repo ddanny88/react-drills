1de1e699e22db1c61a6147071b033edcc87d3bf8
Daniel Dominguez <ddominguez20.dd@gmail.com>
2018-12-03 18:58:28 -0600

    1-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      foods: ['taco', 'pizza', 'cheeseburger'],
+      userInput: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e){
+    this.setState({
+      userInput: e.target.value
+    })
+  }
+
+  render(){
+    let food = this.state.foods.filter((food)=> food.includes(this.state.userInput)).map((food)=> <h2>{food}</h2>) 
+  
+    
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+          onChange={ this.handleChange }
+        />
+          <div>
+            {food}
+          </div>
       </div>
-    );
+    )
   }
 }
-
 export default App;
